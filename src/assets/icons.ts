cf5452f3dce215e48e96acd65e7145f7cf57115f
Nodetizer <nodetizer@gmail.com>
2025-10-13 22:00:26 +0800

    Иконки получше добавил

---
+++ src/assets/icons.ts
@@ -0,0 +1,9 @@
+import Logo from "./icons/Logo.svg";
+import Incoming from "./icons/Incoming.svg";
+// Добавляй сюда остальные иконки по аналогии
+
+export {
+  Logo,
+  Incoming,
+  // остальные иконки
+};
--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./sidebar.css";
+import { Logo, Incoming } from "./../assets/icons";
 
 const Sidebar: React.FC = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-logo-wrapper">
         <Link to="/">
-          <img src="/Logo.svg" alt="Notelab Logo" className="sidebar-logo" />
+          <img src={Logo} alt="Notelab Logo" className="sidebar-logo" />
         </Link>
       </div>
 
       <div className="sidebar-links">
         <Link to="/incoming" className="sidebar-link">
-          <img
-            src="/Incoming.svg"
-            alt="Incoming Icon"
-            className="sidebar-icon"
-          />
+          <img src={Incoming} alt="Incoming Icon" className="sidebar-icon" />
           <span className="sidebar-link-text">Входящие</span>
         </Link>
       </div>
